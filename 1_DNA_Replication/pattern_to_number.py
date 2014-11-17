ce5c54988cea592b1945af2683724b14bc4dda19
John Min <john.c.min@gmail.com>
2014-11-17 09:34:08 -0500

    pattern to number works

--- 1_DNA_Replication/pattern_to_number.py
+++ 1_DNA_Replication/pattern_to_number.py
@@ -10,10 +10,19 @@ def create_lexicon(k):
     return lexicon_num_dict
 
 def pattern_to_number(pattern):
+    if pattern == '':
+        return 0
+    symbol = pattern[-1]
+    pattern = pattern[:-1]
+    return 4 * int(pattern_to_number(pattern[:-1]) + int(symbol_number[symbol]))
+    '''
     k = len(pattern)
     lexicon = create_lexicon(k)
     return lexicon[pattern]
+    '''
 
 if __name__ == '__main__':
+    k_mer_list = ['A', 'C', 'G', 'T']
+    symbol_number = dict(zip(k_mer_list, list(range(4))))
     pattern = sys.stdin.readline().split()[0]
     print (pattern_to_number(pattern))
