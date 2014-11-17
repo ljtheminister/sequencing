1b4b6e1d7c14c72a4dbbfe41d0dd8715254c686c
John Min <john.c.min@gmail.com>
2014-11-17 05:39:28 -0500

    initial commit

---
+++ pattern_count.py
@@ -0,0 +1,18 @@
+
+def pattern_count(text, pattern):
+    count = 0
+    k = len(pattern)
+    for i in range(len(text)-k):
+        if text[i:i+k] == pattern:
+            count += 1
+    return count
+
+with open('dataset_2_6.txt') as f:
+    data = f.read()
+data = data.split('\n')
+dna_string = data[0]
+pattern = data[1]
+
+count = str(pattern_count(dna_string, pattern))
+with open('sub', 'w') as f:
+    f.write(count)
